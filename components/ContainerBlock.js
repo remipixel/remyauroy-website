81f32fc50b6b5f2d4daa725bfcc7835e81918724
Rémi Pixel @ Pixel Digital <39331389+remipixel@users.noreply.github.com>
2021-11-17 14:09:37 +0100

    import from containerblock.js

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -1,11 +1,25 @@
 import React from "react";
-import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect } from 'react'
+import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
+
 export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   const meta = {
     title: "Rémy Auroy - Développeur, UI/UX Designer, Graphiste, Automatisation et Domotique IOT",
--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,7 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
-import { useEffect } from 'react'
-import { useRouter } from "next/router";
-
 
 function MyApp({ Component, pageProps }) {
-
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = url => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <Component {...pageProps} />
